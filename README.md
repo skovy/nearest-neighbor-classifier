e316b669f0abc67615a2dfcad5ea542cf3430b78
Spencer Miskoviak <smiskoviak@gmail.com>
2016-04-04 20:42:53 -0700

    add neighbors as parameter

--- README.md
+++ README.md
@@ -1,11 +1,41 @@
 ### Track Nearest Neighbors Classifier
 
+The purpose of this script is to implement a basic K Nearest Neighbors
+Classifier using an audio summary generated from Echonest. We can then
+classify new tracks using their audio summary and it's nearest neighbors.
+
 #### Install Dependencies
 
-- `sklearn`
-- `numpy`
-- `scipy`
+- `sklearn` - Machine learning
+- `numpy`- Scientific computing
+- `scipy` - Math functions
 - `spotify` - Spotify Python Wrapper
 - `pyechonest` - Echonest Python Wrapper
 
 `pip install sklearn numpy scipy spotify pyechonest`
+
+#### Setup
+
+Relies on the track data generated from Spotify and Echonest in the format
+created [using this script](https://github.com/Skovy/spotify-echonest).
+
+#### Add Environment Variables for Spotify/Echonest API's
+
+This is used for the new track being classified.
+
+```
+export SPOTIPY_CLIENT_ID='your-spotify-client-id'
+export SPOTIPY_CLIENT_SECRET='your-spotify-client-secret'
+export SPOTIPY_REDIRECT_URI='http://localhost'
+export ECHO_NEST_API_KEY='your-echonest-api-key'
+```
+
+#### Run the script
+
+- `python main.py <spotify-uri> <n-training-data>`
+  - `spotify-uri` *(required)*: the Spotify URI of the track to classify
+  - `n-training-data` *(optional)*: the total number of tracks to use as training data
+- Spotify Authentication will open in your default browser (first time only)
+- Grant Access
+- Copy the URL to the terminal when prompted (contains the Spotify code)
+- The track will be classified using existing track data from [this script](https://github.com/Skovy/spotify-echonest))
--- main.py
+++ main.py
@@ -5,38 +5,35 @@ from src.echonest import Echonest
 
 spotify_uri = "" # the Spotify URI to classify
 n_training_data = 10 # the number of samples to train with
+n_neighbors = 1 # the number of neighbors to classify with
 
 if len(sys.argv) > 1:
     spotify_uri = sys.argv[1]
     if len(sys.argv) > 2:
         n_training_data = int(sys.argv[2])
+    if len(sys.argv) > 3:
+        n_neighbors = int(sys.argv[3])
 else:
     print("Please pass a Spotify Track URI (eg: spotify:track:3lByrqVGjmURiNbfeSucjb)")
     sys.exit()
 
-print("Classifying " + str(spotify_uri) + " using " + str(n_training_data) + " training samples")
+print("Classifying " + str(spotify_uri) + " using " + str(n_training_data) + " training samples and " + str(n_neighbors) + " neighbors")
 
 # retrieve the Spotify data to verify the correct track is being used
 sp = Spotify()
 sp_track = sp.load_track(spotify_uri)
-print("Using track '" + sp_track['name'] + "'")
 
 # retrieve the track audio summary
 en = Echonest()
 en_track = en.track_attributes(sp_track).values()
-print("Danceability: " + str(en_track[0])) # printed to verify classification
+
+print("Using track '" + sp_track['name'] + "' with danceability of " + str(en_track[0]))
 
 # find the nearest neighbor based on all of the data aside from the classification
-nn = NearestNeighbor(n_training_data, 3)
+nn = NearestNeighbor(n_training_data, n_neighbors)
 index = nn.nearest_neighbor_index(en_track[1:])
-
-# print the closest track and it's classification
-print(index)
-print(nn.index_to_track_title(index))
-print(nn.index_to_track_classification(index))
-print(sp.spotify_uri_to_url(nn.index_to_track_spotify_uri(index)))
 
 # print the k closest neightbors and distances
 distances, indices = nn.nearest_neighbors(en_track[1:])
-print(distances)
-print(indices)
+for distance, index in zip(distances[0], indices[0]):
+    print("[{0}] '{1}' at distance {2} classified as '{3}' - {4}").format(index, nn.index_to_track_title(index), distance, nn.index_to_track_classification(index), sp.spotify_uri_to_url(nn.index_to_track_spotify_uri(index)))
