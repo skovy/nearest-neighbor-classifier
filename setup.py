b658d2e5238807bc1b30d56c5aebad00aa39ee59
Spencer Miskoviak <smiskoviak@gmail.com>
2016-04-20 17:43:47 -0700

    version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='spotify_nearest_neighbor',
-      version='0.1',
+      version='0.2',
       description='Find the nearest Spotify track',
       url='http://github.com/Skovy/',
       author='Spencer',
